f29b4bb8118d5616048c568d9a0a15ae0a0469cd
Anna-Timoshenko <anna.timoshenko@softeq.com>
2021-10-01 11:48:31 +0300

    fixed

--- Hello/ViewModels/DynamicViewModel.cs
+++ Hello/ViewModels/DynamicViewModel.cs
@@ -35,7 +35,7 @@ namespace Hello.ViewModels
         public bool CheckSwitch
         {
             get => _checkSwitch;
-            private set
+            set
             {
                 Set(ref _checkSwitch, value);
                 ChangeTextSwitch(value);
