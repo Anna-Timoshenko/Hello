4f115e3c394b7d66589a54dc53fd281fa3a5c32b
Anna-Timoshenko <anna.timoshenko@softeq.com>
2021-10-05 11:10:21 +0300

    Delete pet

--- Hello/Models/Pet.cs
+++ Hello/Models/Pet.cs
@@ -1,20 +1,7 @@
-﻿using Android.Graphics;
-using Softeq.XToolkit.Common;
+﻿using Softeq.XToolkit.Common;
 
 namespace Hello.Models
 {
-    public class Pet
-    {
-        public Pet(string name, Color color)
-        {
-            Name = name;
-            ColorDog = color;
-        }
-
-        public string Name { get; set; }
-        public Color ColorDog { get; set; }
-    }
-
     public class PetViewModel : ObservableObject
     {
         private string _name = string.Empty;
